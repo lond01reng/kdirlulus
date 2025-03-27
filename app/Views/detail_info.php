6c611582e5dcf892d509284e2a75ee098be46af9
kang.didikw <kang.didikw@gmail.com>
2025-03-28 06:04:14 +0700

    V.1.0

--- app/Views/detail_info.php
+++ app/Views/detail_info.php
@@ -1,36 +1,52 @@
 <?= $this->extend('template/topm') ?>
 <?= $this->section('css') ?>
+<style>
+.background-icon {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      /* z-index: -1; Pastikan ikon berada di belakang tabel */
+      font-size: 10rem; /* Ukuran ikon bisa disesuaikan */
+      color: rgba(0, 0, 0, 0.15); /* Warna ikon dengan transparansi */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    </style>
 <?= $this->endSection() ?> 
 <?= $this->extend('template/topm') ?>
 <?= $this->section('konten') ?>
   <div class="card-body">
-    <div class="row">
+    <div class="row d-flex justify-content-center align-items-center"">
     <?php if($info->sw_status==1){
       $ucapan="Selamat ";
       $status="LULUS";
-      $bg="purple";
+      $bg="light";
       $ribbon="success";
+      $ico="graduate";
     }
     else{
       $ucapan="Mohon Maaf ";
       $status="MENGULANG";
-      $bg="gray";
+      $bg="secondary";
       $ribbon="dark";
+      $ico="slash";
     }
     ?>
-    <div class="position-relative p-3 bg-<?=$bg;?> d-flex justify-content-center align-items-center">
-      <div class="ribbon-wrapper ribbon-lg">
-        <div class="ribbon bg-<?=$ribbon;?> text-sm">
-          <?=$ucapan;?>
-        </div>
-      </div>
-      <div class="card text-dark">
+      <div class="card text-dark w-50 bg-<?=$bg;?> bg-gradient col-12 col-md-6">
         <div class="card-header d-flex justify-content-center text-center">
           <h3 class="card-title">Pengumuman Kelulusan <?=$sch->sc_nama;?><br>Tahun <?='20'.$info->sw_tapel.'/20'.($info->sw_tapel+1);?></h3>
         </div>
         <div class="card-body">
+          <div class="ribbon-wrapper ribbon-lg">
+          <div class="ribbon bg-<?=$ribbon;?> text-sm">
+            <?=$ucapan;?>
+          </div>
+        </div>
+        <i class="fas fa-user-<?=$ico;?> background-icon"></i>
           <table class="table table-borderless table-sm">
-          <i class="fa fa-star fa-background"></i>
             <tbody>
               <tr>
                 <td>Nama</td><td><?=strtoupper($info->sw_nama);?></td>
@@ -57,7 +73,6 @@
           </table>
         </div>
       </div>
-    </div>
     <?php
   ?>
     </div>

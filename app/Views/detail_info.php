811b9ada1c9ffc3eb2fa0876cfd1fc318533b5ea
lond01reng <kang.didikw@gmail.com>
2025-03-29 21:36:02 +0700

    V.1.0

--- app/Views/detail_info.php
+++ app/Views/detail_info.php
@@ -34,10 +34,11 @@
       $ribbon="dark";
       $ico="slash";
     }
+    $sch=!empty($sch->sc_nama)?$sch->sc_nama:'kdir';
     ?>
       <div class="card text-dark w-50 bg-<?=$bg;?> bg-gradient col-12 col-md-6">
         <div class="card-header d-flex justify-content-center text-center">
-          <h3 class="card-title">Pengumuman Kelulusan <?=$sch->sc_nama;?><br>Tahun <?='20'.$info->sw_tapel.'/20'.($info->sw_tapel+1);?></h3>
+          <h3 class="card-title">Pengumuman Kelulusan <?=$sch;?><br>Tahun <?='20'.$info->sw_tapel.'/20'.($info->sw_tapel+1);?></h3>
         </div>
         <div class="card-body">
           <div class="ribbon-wrapper ribbon-lg">
@@ -61,7 +62,7 @@
                 <td>Kelas </td><td><?=$info->sw_kelas.' ('.$info->sw_jurusan.')';?></td>
               </tr>
               <tr>
-                <td colspan=2>Rapat Kelulusan tahun <?='20'.$info->sw_tapel.'/20'.($info->sw_tapel+1);?> <?=$sch->sc_nama;?> menyatakan: </td>
+                <td colspan=2>Rapat Kelulusan tahun <?='20'.$info->sw_tapel.'/20'.($info->sw_tapel+1);?> <?=$sch;?> menyatakan: </td>
               </tr>
               <tr>
                 <td colspan=2 class="text-center">
